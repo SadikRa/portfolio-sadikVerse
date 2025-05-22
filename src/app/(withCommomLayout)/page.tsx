0082a825ca35618c4ea1387eb24fa98ab8805865
SadikRa <sadikrahman494@gmail.com>
2025-05-22 22:24:19 +0600

    nop not good

--- src/app/(withCommomLayout)/page.tsx
+++ src/app/(withCommomLayout)/page.tsx
@@ -1,41 +1,31 @@
-import Home from "@/components/Home/Cube/Cube";
+import SkillShowCase from "@/components/Home/Skills/SkillShowCase";
+import Banner from "@/components/Home/Banner/Banner";
+import React from "react";
+import FeaturesProject from "@/components/Home/FeaturesProject/FeaturesProject";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const HomePage = async () => {
   return (
-    <div>
-      <Home />
+    <div className="mt-16">
+      <div>
+        <Banner />
+      </div>
+      <div>
+        <SkillShowCase />
+      </div>
+      <div className="my-24">
+        <h1 className="text-2xl font-bold text-center my-6 text-gray-900 dark:text-white ">
+          Features Project
+        </h1>
+        <FeaturesProject />
+        <div className="text-center mt-14">
+          <Link href="/project">
+            <Button>See All Projects</Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
 export default HomePage;
-// import SkillShowCase from "@/components/Home/Skills/SkillShowCase";
-// import Banner from "@/components/Home/Banner/Banner";
-// import React from "react";
-// import FeaturesProject from "@/components/Home/FeaturesProject/FeaturesProject";
-// import Link from "next/link";
-// import { Button } from "@/components/ui/button";
-
-// const HomePage = async () => {
-//   return (
-//     <div className="mt-16">
-//       <div>
-//         <Banner />
-//       </div>
-//       <div>
-//         <SkillShowCase />
-//       </div>
-//       <div className="my-24">
-//         <h1 className="text-2xl font-bold text-center my-6 text-gray-900 dark:text-white ">
-//           Features Project
-//         </h1>
-//         <FeaturesProject />
-//         <div className="text-center mt-14">
-//           <Link href="/project">
-//             <Button>See All Projects</Button>
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-// export default HomePage;
